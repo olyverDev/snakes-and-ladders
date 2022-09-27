3d6b90fee27dd55874f24e4dcb75e954999cf6bb
Nikita Abrazhevich <neron2150@gmail.com>
2022-09-27 22:42:15 +0200

    rolling the dice

--- src/components/GameComponent/index.tsx
+++ src/components/GameComponent/index.tsx
@@ -1,12 +1,20 @@
-import { useCallback, useEffect, useMemo, useRef } from 'react';
+import { useCallback, useEffect, useMemo, useRef, useState } from 'react';
+
+import AudioPlayer from '../AudioPlayer';
+import Dice from '../Dice';
 import Game from '../../game';
 import { Cell } from '../../game/Cell';
 import { useWindowResize } from '../../utils';
 import './GameComponent.css';
 
 function GameComponent() {
+  const [history, setHistory] = useState([0]);
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
   const game = useMemo(() => new Game(), []);
+
+  const onRoll = useCallback((newValue: number) => {
+    setHistory((previous) => [...previous, newValue]);
+  }, []);
 
   const onResize = useCallback(() => {
     if (canvasRef.current && game) {
@@ -27,9 +35,17 @@ function GameComponent() {
   }, [onResize]);
 
   useWindowResize(onResize);
+
   return (
-    <div className="GameComponent">
-      <canvas ref={canvasRef} className="Canvas" />
+    <div className="GameContainer">
+      <div className="GameComponent">
+        {history}
+        <canvas ref={canvasRef} className="Canvas" />
+      </div>
+      <div className="SideControls">
+        <AudioPlayer />
+        <Dice onRoll={onRoll} />
+      </div>
     </div>
   );
 }
