fbabd9e3dacb6f6102b91a36f22eb10922ec840d
olyverDev <o.veremejchik@gmail.com>
2022-11-28 15:33:47 +0300

    Game UI: background, menu, loading, buttons

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,17 +1,32 @@
+import { useEffect, useMemo, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
 import './Menu.css';
 
 type MenuProps = {
   loading: boolean;
-  play: () => unknown;
+  onPlayPress: () => unknown;
 };
 
-function Menu({ loading, play }: MenuProps) {
+function Menu({ loading, onPlayPress }: MenuProps) {
   const { t } = useTranslation();
+  const [coverVisibility, setCoverVisibility] = useState<'hidden' | 'visible'>('visible');
+
+  useEffect(() => {
+    setTimeout(() => {
+      if (loading) return;
+      setCoverVisibility('hidden');
+    }, 2500);
+  }, [loading]);
+
+  const coverInlineStyles = useMemo(() => ({
+    visibility: coverVisibility,
+  }), [coverVisibility]);
+
   return (
     <div className="Menu">
-      <button disabled={loading} onClick={play}>{loading ? t('loading') : t('play')}</button>
+      <div style={coverInlineStyles} className="Cover" />
+      {coverVisibility === 'hidden' && <button className="PlayButton" onClick={onPlayPress}>{t('play')}</button>}
     </div>
   );
 }
