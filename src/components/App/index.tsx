6d3461c2c8181bc91c9a064ede40f2fcee348037
Nikita Abrazhevich <neron2150@gmail.com>
2022-12-18 22:10:02 +0100

    WIP: game restart logic

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,16 +1,88 @@
 import '@fontsource/nunito';
 import '@fontsource/jost/300.css';
 
-import { ReactElement, useEffect, useMemo, useState } from 'react';
+import { ReactElement, useEffect, useMemo, useRef, useState } from 'react';
 import { AnalyticsEvent, logAnalyticsEvent } from '../../firebase';
 
 import { GameImagesService } from '../../gameImagesService';
 import GameComponent from '../GameComponent';
 import Menu from '../Menu';
 import './App.css';
+import Game from '../../game';
+import {
+  DEFAULT_MODALS_LINKED_LIST,
+  getInitialModalsLinkedList,
+  getInitialPlayersConfig,
+  Modals,
+} from '../../utils';
+import GameRuleModal from '../Modals/GameRuleModal';
+import EndGameModal from '../Modals/EndGameModal';
+import PromoGreetingModal from '../Modals/PromoGreetingModal';
+import PromoEndGameModal from '../Modals/PromoEndGameModal';
+import SelectGameModeModal from '../Modals/SelectGameModeModal';
+import { GameEvent } from '../../game/GameEvent';
+import { useTranslation } from 'react-i18next';
+
+// FIXME: start game only after game mode selection and assigning needed players config OR greeting in promo mode
+const players = getInitialPlayersConfig();
+new Game(players);
 
 function App() {
+  const modalsLinkedListRef = useRef(getInitialModalsLinkedList());
+  const availableModals = Object.keys(modalsLinkedListRef.current);
   const { loaded: imagesLoaded } = GameImagesService.useLoad();
+  const [isGameEnd, setGameEnd] = useState(false);
+  const initialActiveModalId = availableModals.find((key) =>
+    Boolean(modalsLinkedListRef.current[key]?.initial)
+  );
+  const [activeModalId, setActiveModalId] = useState<string | null | undefined>();
+
+  useEffect(() => {
+    if (isGameEnd) {
+      new Game(players);
+      setGameEnd(false);
+    }
+  }, [isGameEnd]);
+
+  const { t } = useTranslation();
+
+  useEffect(() => {
+    const eventId = GameEvent.addListener('gameEnd', (payload) => {
+      alert(t('gameEndedForPlayer', { name: payload.player }));
+
+      const realPlayers = Game.playerConfig.filter(player => !player.automatic);
+
+      if (realPlayers.length === 0) {
+        setGameEnd(true);
+        const endGameModalId = availableModals.find(key => Boolean(modalsLinkedListRef.current[key]?.gameEnding));
+        setActiveModalId(endGameModalId);
+      }
+    });
+
+    return () => {
+      GameEvent.removeListener(eventId);
+    };
+  }, []);
+
+  const closeModalFactory =
+    (callback?: (result?: unknown) => unknown) => (result?: unknown) => {
+      if (activeModalId) {
+        setActiveModalId(modalsLinkedListRef.current[activeModalId]?.next);
+      }
+
+      if (callback) callback(result);
+    };
+
+  const handleCloseEndGameModal = closeModalFactory(() => {
+    modalsLinkedListRef.current = DEFAULT_MODALS_LINKED_LIST;
+    Game.playerConfig = players;
+    setGameEnd(false);
+  });
+
+  const handleCloseSelectGameModeModal = closeModalFactory((result) => {
+    // Game.playerConfig = players;
+    // setGameEnd(false);
+  });
 
   const SCREENS = useMemo(
     () => ({
@@ -20,21 +92,46 @@ function App() {
           onPlayPress={() => {
             logAnalyticsEvent(AnalyticsEvent.PressPlay);
             setCurrentRoute(SCREENS.GAME);
+            setActiveModalId(initialActiveModalId);
           }}
         />
       ),
-      GAME: <GameComponent />,
+      GAME: (
+        <GameComponent
+          players={players}
+          isGameEnd={isGameEnd}
+        />
+      ),
     }),
-    [imagesLoaded]
+    [imagesLoaded, players, isGameEnd, setGameEnd]
   );
-
   const [currentRoute, setCurrentRoute] = useState<ReactElement>(SCREENS.MENU);
-
   useEffect(() => {
     setCurrentRoute(SCREENS.MENU);
   }, [SCREENS]);
 
-  return <div className="App">{currentRoute}</div>;
+  if (isGameEnd) return null;
+
+  return (
+    <>
+      {activeModalId === Modals.GameRuleModal && (
+        <GameRuleModal onClose={closeModalFactory()} />
+      )}
+      {activeModalId === Modals.SelectGameModeModal && (
+        <SelectGameModeModal onClose={handleCloseSelectGameModeModal} />
+      )}
+      {activeModalId === Modals.PromoGreetingModal && (
+        <PromoGreetingModal onClose={closeModalFactory()} />
+      )}
+      {activeModalId === Modals.EndGameModal && (
+        <EndGameModal onClose={handleCloseEndGameModal} />
+      )}
+      {activeModalId === Modals.PromoEndGameModal && (
+        <PromoEndGameModal isWinner onClose={closeModalFactory()} />
+      )}
+      <div className="App">{currentRoute}</div>;
+    </>
+  );
 }
 
 export default App;
--- src/components/GameComponent/index.tsx
+++ src/components/GameComponent/index.tsx
@@ -1,5 +1,10 @@
-import { useCallback, useEffect, useMemo, useRef, useState } from 'react';
-import { useTranslation } from 'react-i18next';
+import {
+  useCallback,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from 'react';
 
 import AudioPlayer from '../AudioPlayer';
 import Dice, { DiceRef } from '../Dice';
@@ -7,61 +12,27 @@ import Game from '../../game';
 import { Cell } from '../../game/Cell';
 import { gameLoopFactory } from '../../gameLoop';
 import {
-  getInitialPlayersConfig,
-  Modals,
-  getInitialModalsLinkedList,
-  DEFALT_MODALS_LINKED_LIST,
-  SINGLE_PLAYER_CONFIG,
-  TWO_PLAYERS_CONFIG,
   useGameSounds,
   useWindowResize,
 } from '../../utils';
 import './GameComponent.css';
 import { GameEvent } from '../../game/GameEvent';
 
-import GameRuleModal from '../Modals/GameRuleModal';
-import EndGameModal from '../Modals/EndGameModal';
-import PromoGreetingModal from '../Modals/PromoGreetingModal';
-import PromoEndGameModal from '../Modals/PromoEndGameModal';
-import SelectGameModeModal from '../Modals/SelectGameModeModal';
+import { PlayerConfig } from '../../game/Game';
 
-// FIXME: start game only after game mode selectino and assigning needed players config OR greeting in promo mode
-const Players = getInitialPlayersConfig();
-new Game(Players);
+type GameComponentPropsType = {
+  players: PlayerConfig[];
+  isGameEnd: boolean;
+};
 
-function GameComponent() {
-  const [history, setHistory] = useState([0]);
-  const { t } = useTranslation();
+function GameComponent({
+  isGameEnd,
+}: GameComponentPropsType) {
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
   const game = useMemo(() => Game.object, []);
   const gameSounds = useGameSounds();
 
-
   const [moving, setMoving] = useState(false);
-  const [isGameEnd, setGameEnd] = useState(false);
-
-  const modalsLinkedListRef = useRef(getInitialModalsLinkedList());
-  const availableModals = Object.keys(modalsLinkedListRef.current);
-  const initialActiveModalId = availableModals.find(key => Boolean(modalsLinkedListRef.current[key]?.initial));
-  const [activeModalId, setActiveModalId] = useState<string | null | undefined>(initialActiveModalId);
-
-  useEffect(() => {
-    const eventId = GameEvent.addListener('gameEnd', (payload) => {
-      alert(t('gameEndedForPlayer', { name: payload.player }));
-
-      const realPlayers = Game.playerConfig.filter(player => !player.automatic);
-
-      if (realPlayers.length === 0) {
-        setGameEnd(true);
-        const endGameModalId = availableModals.find(key => Boolean(modalsLinkedListRef.current[key]?.gameEnding));
-        setActiveModalId(endGameModalId);
-      }
-    });
-
-    return () => {
-      GameEvent.removeListener(eventId);
-    };
-  }, []);
 
   useEffect(() => {
     const userStartMoveId = GameEvent.addListener('userStartMove', () => {
@@ -79,13 +50,17 @@ function GameComponent() {
 
   useEffect(() => {
     const eventId = GameEvent.addListener('nextTurn', () => {
-      const turnIndex = Game.playerConfig.map(p => p.key).indexOf(game.activePlayerKey);
+      const turnIndex = Game.playerConfig
+        .map((p) => p.key)
+        .indexOf(game.activePlayerKey);
       const isLastPlayer = turnIndex === Game.playerConfig.length - 1;
       const nextIndex = isLastPlayer ? 0 : turnIndex + 1;
       const nextPlayer = Game.playerConfig[nextIndex];
       game.setActivePlayerKey(nextPlayer.key);
 
-      const realPlayers = Game.playerConfig.filter(player => !player.automatic);
+      const realPlayers = Game.playerConfig.filter(
+        (player) => !player.automatic
+      );
 
       if (realPlayers.length === 0) {
         // NOTE: no need to roll dice as only bots left
@@ -111,14 +86,10 @@ function GameComponent() {
     game.loopInitialized = true;
   }, [game]);
 
-  const onRoll = useCallback(
-    (countMoves: number) => {
-      GameEvent.fire('userStartMove');
-      setHistory((previous) => [...previous, countMoves]);
-      game.moveUser({ countMoves });
-    },
-    [],
-  );
+  const onRoll = useCallback((countMoves: number) => {
+    GameEvent.fire('userStartMove');
+    game.moveUser({ countMoves });
+  }, []);
 
   const onResize = useCallback(() => {
     if (canvasRef.current && game) {
@@ -130,38 +101,12 @@ function GameComponent() {
   }, [game]);
 
   useEffect(() => {
-    if (game.isInitialized || !canvasRef.current || !game || !gameSounds) return;
+    if (game.isInitialized || !canvasRef.current || !game || !gameSounds)
+      return;
     game.init(canvasRef.current, gameSounds);
-    onResize();
-  }, [onResize, gameSounds]);
+  }, [gameSounds]);
 
   useWindowResize(onResize);
-
-
-  const closeModalFactory = (callback?: (result?: unknown) => unknown) => (result?: unknown) => {
-    if (activeModalId) {
-      setActiveModalId(modalsLinkedListRef.current[activeModalId]?.next);
-    }
-
-    if (callback) callback(result);
-  };
-
-  const handleCloseEndGameModal = closeModalFactory(() => {
-    // TODO: restart game properly
-    modalsLinkedListRef.current = DEFALT_MODALS_LINKED_LIST;
-    Game.playerConfig = Players;
-    onResize();
-    setGameEnd(false);
-  });
-
-  const handleCloseSelectGameModeModal = closeModalFactory((result) => {
-    // TODO: restart game properly
-    // modalsLinkedListRef.current = DEFALT_MODALS_LINKED_LIST;
-    // const newPlayersConfig = result === 'vsBot' ? SINGLE_PLAYER_CONFIG : TWO_PLAYERS_CONFIG;
-    // Game.playerConfig = newPlayersConfig;
-    // onResize();
-    // setGameEnd(false);
-  });
 
   return (
     <div className="GameContainer">
@@ -172,12 +117,6 @@ function GameComponent() {
         <AudioPlayer />
         {!isGameEnd && <Dice disabled={moving} onRoll={onRoll} />}
       </div>
-
-      {activeModalId === Modals.GameRuleModal && <GameRuleModal onClose={closeModalFactory()} />}
-      {activeModalId === Modals.SelectGameModeModal && <SelectGameModeModal onClose={handleCloseSelectGameModeModal} />}
-      {activeModalId === Modals.PromoGreetingModal && <PromoGreetingModal onClose={closeModalFactory()} />}
-      {activeModalId === Modals.EndGameModal && <EndGameModal onClose={handleCloseEndGameModal} />}
-      {activeModalId === Modals.PromoEndGameModal && <PromoEndGameModal isWinner onClose={closeModalFactory()} />}
     </div>
   );
 }
